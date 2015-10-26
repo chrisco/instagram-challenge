def04c7aacb6e7f96eeddebabe2c59063e1323d0
Christopher Comella <git.chrisco@gmail.com>
2015-10-26 23:52:52 +0100

    Heroku troubleshooting: move AWS bucket outside s3_credentials hash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,8 @@ Rails.application.configure do
   # From https://github.com/thoughtbot/paperclip/wiki/Paperclip-with-Amazon-S3
   config.paperclip_defaults = {
       :storage => :s3,
-      :region => 'us-east-1',
+      :bucket => ENV['S3_BUCKET_NAME'],
       :s3_credentials => {
-          :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
