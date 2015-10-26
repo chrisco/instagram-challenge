67a9dd6617922b2166d18edb80494f9f2da1a4ae
Christopher Comella <git.chrisco@gmail.com>
2015-10-26 22:06:31 +0100

    edit production.rb and aws.yml as part of Heroku troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -79,7 +79,11 @@ Rails.application.configure do
 
   # From https://github.com/thoughtbot/paperclip/wiki/Paperclip-with-Amazon-S3
   config.paperclip_defaults = {
-    :storage => :s3,
-    :bucket => ENV['S3_BUCKET_NAME']
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
   }
 end
