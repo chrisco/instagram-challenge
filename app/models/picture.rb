0607f6c25f7c042158b825e5ab3bffdefba1379c
Christopher Comella <git.chrisco@gmail.com>
2015-10-28 22:09:29 +0100

    S3 troubleshoot: specify S3 info in picture.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,11 @@ class Picture < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: ":style/monkey.png"
+  has_attached_file :image,
+                    styles: {medium: "300x300>", thumb: "100x100>"},
+                    :storage => :s3,
+                    :bucket  => ENV['S3_BUCKET_NAME'],
+                    default_url: ':style/monkey.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 500.kilobytes
 end
